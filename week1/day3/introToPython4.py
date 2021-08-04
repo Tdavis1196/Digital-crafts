c63e0a661f1c669985184b0e9456f6f0bc4715b1
Timothy Davis <d.keyshawn96@yahoo.com>
2021-08-04 16:58:43 -0400

    Even Odd Assignment

--- week1/day3/introToPython4.py
+++ week1/day3/introToPython4.py
@@ -1,14 +1,6 @@
-# Conditionals
-
-print("Welcome to this program")
-firstName = input("Hey, what is your first name?")
-length_of_first_name = len(firstName)
-print(length_of_first_name)
-if length_of_first_name <= 0:
-    print("Please enter at least one character")
+firstresponse = int(input("what is your number?"))
+if (firstresponse % 2) == 0:
+    print("{0} is Even number")
 else:
-    print(f"Hello {firstName}, welcome!")
-    pass
-
-
+    print("{0} is Odd number") 
     
