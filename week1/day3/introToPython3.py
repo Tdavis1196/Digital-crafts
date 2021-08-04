5d779cf116bf6b3c3529b286006b5f2c42d1a195
Timothy Davis <d.keyshawn96@yahoo.com>
2021-08-04 17:20:54 -0400

    Even Odd

--- week1/day3/introToPython3.py
+++ week1/day3/introToPython3.py
@@ -1,2 +1,5 @@
-greeting = input("Are you from Atlanta?")
-print("Howdy " + greeting)
+firstresponse = int(input("Enter a number:"))
+if firstresponse % 2 == 0:
+    print("{0} is an Even number")
+else:
+    print("{0} is an Odd number")
